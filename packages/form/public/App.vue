7016866bbc78314110a28d49d76c4873495a9186
Randy Houlahan <randy.houlahan@cbd.int>
2020-09-16 11:57:10 -0400

    save

---
+++ packages/form/public/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AAForm :options="{ }" force-env="development"/>
+  </div>
+</template>
+
+<script>
+import '@scbd/www-css'
+
+import AAForm from '../src/index.vue'
+
+export default {
+  name      : 'App',
+  components: {
+    AAForm
+  }
+}
+</script>
+
